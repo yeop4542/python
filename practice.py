2a8c74037b5634d59a4cf8ff7bcbdeaa8c107c37
yeop4542 <81512990+yeop4542@users.noreply.github.com>
2021-03-28 19:59:32 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+# Quiz 1
+
+station = "사당"
+print(station, "행 열차가 들어오고 있습니다.") 
+station = "신도림"
+print(station, "행 열차가 들어오고 있습니다.") 
+station = "인천공항"
+print(station, "행 열차가 들어오고 있습니다.") 
+
+# Quiz 2
+
+from random import *
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월",str(date),"일로 선정되었습니다")
