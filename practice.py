fa060362a8ff3e59628a17899b0b7ef4c69c295c
yeop4542 <81512990+yeop4542@users.noreply.github.com>
2021-04-05 02:46:46 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -12,3 +12,12 @@ print(station, "행 열차가 들어오고 있습니다.")
 from random import *
 date = randint(4, 28)
 print("오프라인 스터디 모임 날짜는 매월",str(date),"일로 선정되었습니다")
+
+# Quiz 3
+
+adress = "http://naver.com"
+x = adress.replace("http://", "")
+y = x.index(".")
+z = x[:y]
+password = z[:3] + str(y) + str(z.count("e")) + "!"
+print("{}의 비밀번호는 {}입니다." .format(adress, password))
